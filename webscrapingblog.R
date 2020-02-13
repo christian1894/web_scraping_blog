e270fd1691455f81d48d845b735e8da4c8fe9c3b
Christian Paez <christian.paez@pluriza.com>
2020-02-13 13:34:49 -0500

    getting books urls

--- webscrapingblog.R
+++ webscrapingblog.R
@@ -11,3 +11,30 @@ node<-html_node(page, selector)
 node_text<-html_text(node)
 node_links<-html_attr(node, "href")
 node_links
+
+full_url<-paste0("http://books.toscrape.com/",node_links)
+full_url
+
+library(stringr)
+single_page = "http://books.toscrape.com/catalogue/page-3.html"
+pages_list<-c(1:50)
+all_pages<-str_replace(single_page, "page-3", paste0("page-",pages_list))
+all_pages
+
+#URL LIST AFTER PAGINATION
+linksGetter<-function(url){
+  selector = "article > h3 > a"
+  pagina<-read_html(url)
+  nodo<-html_nodes(pagina, selector)
+  nodo_text<-html_text(nodo)
+  nodo_links<-html_attr(nodo, "href")
+  nodo_links
+}
+#Function test
+first_page_books = linksGetter(pag[1])
+first_page_books
+#All books URls
+books_urls<-sapply(all_pages, linksGetter)
+books_urls<- as.vector(books_urls)
+books_urls<-paste0("http://books.toscrape.com/catalogue/", books_urls)
+books_urls
