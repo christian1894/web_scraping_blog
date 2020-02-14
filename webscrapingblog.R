8c918a9afd137b31bf5e9248d54f96a27f12d495
Christian Paez <christian.paez@pluriza.com>
2020-02-14 15:11:59 -0500

    full details

--- webscrapingblog.R
+++ webscrapingblog.R
@@ -4,10 +4,10 @@ library(rvest)
 library(purrr)
 #read site url
 url = "http://books.toscrape.com/index.html"
-page<-read_html(url)
+web_page<-read_html(url)
 #scraping test to extract each product's href`
 selector = "li.col-xs-6:nth-child(1) > article:nth-child(1) > h3:nth-child(3) > a:nth-child(1)"
-node<-html_node(page, selector)
+node<-html_node(web_page, selector)
 node_text<-html_text(node)
 node_links<-html_attr(node, "href")
 node_links
@@ -38,3 +38,88 @@ books_urls<-sapply(all_pages, linksGetter)
 books_urls<- as.vector(books_urls)
 books_urls<-paste0("http://books.toscrape.com/catalogue/", books_urls)
 books_urls
+
+url = "http://books.toscrape.com/catalogue/tipping-the-velvet_999/index.html"
+web_page<-read_html(url)
+#extracting book's name
+name_selector<-"h1"
+name_node<-html_node(web_page, name_selector)
+name_text<-html_text(name_node)
+name_text
+
+#extracting book's price
+price_selector <-"p.price_color"
+price_node<-html_node(web_page, price_selector)
+price_text<-html_text(price_node)
+price_text
+
+#extracting review as number
+reviews<-"p.star-rating"
+reviews_node<-html_node(web_page, reviews)
+reviews_text<-html_attr(reviews_node, "class")
+reviews_text = substr(reviews_text, 13, 17)
+reviews_number = switch(reviews_text, "Zero" = 0,"One" = 1, "Two" = 2, "Three" = 3, "Four" = 4, "Five" = 5,)
+reviews_number
+
+#extracting product details table
+#PRODUCT DETAIL
+details_selector = ".table"
+details_node<-html_node(web_page, details_selector)
+table_node_table<-html_table(details_node)
+table_values<-table_tab$X2
+table_dataframe = data.frame(t(table_values))
+table_names = table_node_table$X1
+colnames(table_dataframe) = table_names
+table_dataframe
+View(table_dataframe)
+str(table_dataframe)
+
+#book details result
+single_book_results = c(name_text, price_text, reviews_number, as.character(table_dataframe$Availability))
+single_book_results
+
+# BOOK DETAILS FUNCTION
+getDetails = function(url){
+  web_page = read_html(url)
+  #BOOK NAME
+  name_selector<-"h1"
+  name_node<-html_node(web_page, name_selector)
+  name_text<-html_text(name_node)
+  
+  #BOOK PRICE
+  price_selector <-"p.price_color"
+  price_node<-html_node(web_page, price_selector)
+  price_text<-html_text(price_node)
+  
+  #BOOK REVIEWS
+  review<-"p.star-rating"
+  review_node<-html_node(web_page, review)
+  review_text<-html_attr(review_node, "class")
+  review_text = substr(review_text, 13, 17)
+  review_number = switch(review_text, "Zero" = 0,"One" = 1, "Two" = 2, "Three" = 3, "Four" = 4, "Five" = 5,)
+  
+  #PRODUCT DETAIL
+  details_selector = ".table"
+  details_node<-html_node(web_page, details_selector)
+  table_node_table<-html_table(details_node)
+  table_values<-table_tab$X2
+  table_dataframe = data.frame(t(table_values))
+  table_names = table_node_table$X1
+  colnames(table_dataframe) = table_names
+  book = c(name_text, review_number, price_text, as.character(table$Availability))
+}
+
+single_book_test = getDetails(books_urls[1])
+single_book_test
+
+#Scraping result
+
+scraping_results = sapply(books_urls, getDetails)
+
+
+#CREATE DATAFRAME
+scraping_results = t(scraping_results)
+colnames(scraping_results) = c("Book Name", "Rating", "Price", "Availability")
+rownames(scraping_results) = c(1:1000)
+write.csv(scraping_results, file="bookScrapingResults")
+
